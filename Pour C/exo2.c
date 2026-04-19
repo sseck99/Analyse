c839acf813093493769e81dbe55eb2d7d9604eb1
Serigne Seck <sseck9912@gmail.com>
2026-04-19 18:34:18 +0000

    ajout de ficher

--- Pour C/exo2.c
+++ Pour C/exo2.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 
-int main(viod){
+int main(){
+
+
 
     int annee ;
     printf("veillez entre l'annee:");
     scanf("%d",&annee);
     if ( annee % 4==0 && annee % 100 !=0 || annee % 400==0)
     {
-        printf("l'annee %d est bissextille",annee);
+        printf("l'annee %d est bissextille\n",annee);
     }
     else 
     {
-        printf("l'annee %d n'est pas bissextille",annee);
+        printf("l'annee %d n'est pas bissextille\n",annee);
     }
     return 0;
 }
