772337082f5edf8a21f58a6d441ba44e0a393ac1
Serigne Seck <sseck9912@gmail.com>
2026-04-26 16:50:48 +0000

    ajout ficher

---
+++ Pour C/TD/exercice5.c
@@ -0,0 +1,13 @@
+ # include<stdio.h>
+
+ int main(void)
+ {
+    float pht ;
+    float ttc;
+    const float tva = 0.196 ;
+    printf("veuillez entre le prix hors taxes:");
+    scanf("%f",&pht);
+    printf("le prix ttc est de: %2.f",ttc = pht*(1+ tva)):
+    
+    return 0;
+ }
