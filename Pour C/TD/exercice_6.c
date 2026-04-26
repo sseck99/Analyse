86c7465db0049b9bfa8be878233c6b2a88bbf00e
Serigne Seck <sseck9912@gmail.com>
2026-04-26 21:01:14 +0000

    Ajout du fichier exercice5_2.c pour le calcul du prix TTC

--- Pour C/TD/exercice_6.c
+++ Pour C/TD/exercice_6.c
@@ -4,23 +4,32 @@ int main(void)
 {
     float a, b, c, X1, X2, X0, delta;
     
-        printf("Entrez a, b, c : ");
-        scanf("%lf %lf %lf", &a, &b, &c);
 
-    delta = b*b-4*a*c;
+    printf("Résolutios de l'équation ax^2 + bx + c = 0\n");
+
+    printf("Veuillez saisir la valeur de a: ");
+    scanf("%f",&a);
+    printf("Veuillez saisir la valeur de b");
+    scanf("%f",&b);
+    printf("Veuillez saisir la valeur de c");
+    scanf("%f",&c);
+
+    delta = b*b -4*a*c;
 
     if(delta > 0)
     {
-        X1 = (-b+sqrt(delta))/2*a;
-        X2 = (-b-sqrt(delta))/2*a;
+        X1 = (-b+sqrt(delta))/(2*a);
+        X2 = (-b-sqrt(delta))/(2*a);
         printf("Equation admet deux solution X1=%f et X2=%f\n",X1,X2);
 
-    }else if (delta = 0)
+    }
+    else if (delta == 0)
     {
-        X0 = -b/2*a ;
-        printf("Equation admet une unique solution X0=%f\n",X0);
-
-    }else if (delta < 0)
+       X0 = -b/(2*a) ;
+        printf("Equation admet une unique solution X0=%f\n",X0); 
+        
+    }
+    else
     {
         printf("Equation admet pas de solution dans R\n");
     }
